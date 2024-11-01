e6c4826bbd959337b1ab7a7733f872ea66ff48fe
tomernegad <tomernegad@gmail.com>
2024-11-01 12:08:56 +0200

    Design for the delete button

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -1,8 +1,14 @@
 import styles from "./todoItem.module.css";
 export default function TodoItem({ item }) {
+  function handleDelete() {}
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{item}</div>
+      <div className={styles.itemName}>
+        {item}{" "}
+        <button onClick={handleDelete} className={styles.deleteButton}>
+          x
+        </button>
+      </div>
       <hr className={styles.line} />
     </div>
   );
