a699a96e3485671b9a7a02603952f12b258977fd
tomernegad <tomernegad@gmail.com>
2024-11-01 16:35:50 +0200

    .

--- todoapp/src/components/TodoItem.jsx
+++ todoapp/src/components/TodoItem.jsx
@@ -1,6 +1,8 @@
 import styles from "./todoItem.module.css";
 export default function TodoItem({ item }) {
-  function handleDelete() {}
+  function handleDelete() {
+    console.log("Delete activated");
+  }
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
